a702f5cf238f71d1f8202d8a370e72e5db97f872
aniki219 <aniki219@gmail.com>
2024-06-22 16:50:11 -0700

    apparently we dont need an API key?

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,11 +5,22 @@ import CardDisplay from "./components/CardDisplay";
 import process from "process";
 import { render } from "react-dom";
 import CardDisplayFrame from "./components/CardDisplayFrame";
+import "./styles.css"
 
 const Popup = () => {
     const [cardNames, setCardNames] = useState<string[]>([])
 
     const [check, setCheck] = useState(false);
+    const [apiKey, setAPIKey] = useState<string>()
+
+    useEffect(() => {
+        chrome.storage.sync.get(
+            "apiKey",
+            (data) => {
+                setAPIKey(data.apiKey);
+            }
+        );
+    }, []);
 
     useEffect(() => {
         chrome.storage.local.get(["cardNames"], (data) => {
@@ -22,27 +33,24 @@ const Popup = () => {
                 resyncCardNames();
             }
         })
-    }, []);
+    }, [apiKey]);
 
     const [fetchingAllCardNames, setFetchingAllCardNames] = useState(false);
-    const [fetchPage, setFetchPage] = useState(1);
-    const [pagesTotal, setPagesTotal] = useState(20);
+    const [pagesLoaded, setPagesLoaded] =
+        useState<{ loaded: number, totalPages: number }>({ loaded: 0, totalPages: 0 });
 
     const getAllStandardCardNames = async (totalPages: number) => {
         setFetchingAllCardNames(true);
+        var loaded = 0;
 
         const promises = (new Array(totalPages)).fill(0).map((_, i) => {
             return new Promise<CardResults>((resolve) => {
                 const url = `https://api.pokemontcg.io/v2/cards?page=${i + 1}&q=legalities.standard:legal`;
-                fetch(url, {
-                    method: "GET",
-                    headers: {
-                        "X-Auth-Token": process.env.POKEMON_SECRET as string
-                    }
-                })
+                fetch(url)
                     .then(resp => resp.json())
                     .then(function (res: CardResults) {
-                        console.log("promise " + i + ": ", res);
+                        loaded++;
+                        setPagesLoaded({ ...pagesLoaded, loaded });
                         return resolve(res);
                     });
             })
@@ -65,30 +73,29 @@ const Popup = () => {
                 chrome.storage.local.set({ "cardNames": alphabetizedNames });
                 setCardNames(alphabetizedNames);
                 setFetchingAllCardNames(false);
-            }
-            );
+                setStatus("Finished Syncing");
+                setTimeout(() => {
+                    setStatus("");
+                }, 4000);
+            });
     }
     const resyncCardNames = async () => fetchAllCardNames();
     const checkForNewSet = async () => fetchAllCardNames(true);
 
     const fetchAllCardNames = async (onlyIfNewSetExists = false) => {
+        setStatus("Checking for latest set...");
         chrome.storage.local.get(["currentSet"], async (set) => {
             const { currentSet } = set;
             const url = `https://api.pokemontcg.io/v2/sets?q=legalities.standard:legal`;
-            fetch(url, {
-                method: "GET",
-                headers: {
-                    "X-Auth-Token": process.env.POKEMON_SECRET as string
-                }
-            })
+            fetch(url)
                 .then(resp => resp.json())
                 .then(function (setsData: { data: { name: string, total: number }[] }) {
                     const latestSet = setsData.data.at(-1)?.name as string;
-                    console.log("current set: " + currentSet, "latest set: " + latestSet);
                     const totalCards = setsData.data
                         .map(d => d.total)
                         .reduce((a, b) => a + b, 0);
                     const totalPages = Math.ceil(totalCards / 250);
+                    setPagesLoaded({ ...pagesLoaded, totalPages });
                     if (currentSet !== latestSet || !onlyIfNewSetExists) {
                         chrome.storage.local.set({ "currentSet": latestSet });
                         if (currentSet !== latestSet) {
@@ -97,17 +104,38 @@ const Popup = () => {
                             setFetchingReason("Resyncing card names up to set: " + latestSet);
                         }
                         getAllStandardCardNames(totalPages);
+                    } else {
+                        setStatus("")
                     }
                 });
         });
     }
 
     const [fetchingReason, setFetchingReason] = useState("Initializing Card Data");
+    const [status, setStatus] = useState<string>();
+    const [showStatusBar, setShowStatusBar] = useState(false);
+
+    useEffect(() => {
+        if (status && status.length > 0) {
+            setShowStatusBar(true);
+        } else {
+            setShowStatusBar(false);
+        }
+    }, [status])
 
     const showFetchStatus = () => {
         if (fetchingAllCardNames) {
             return (
-                <p>{fetchingReason}...</p>
+                <div className="statusBar">
+                    <p>{fetchingReason} {pagesLoaded.totalPages > 0 ? `${pagesLoaded.loaded} / ${pagesLoaded.totalPages}` : ""}...</p>
+                </div>
+            )
+        }
+        if (status && status.length > 0) {
+            return (
+                <div className="statusBar">
+                    <p className="fadeOut">{status}</p>
+                </div>
             )
         }
     }
@@ -119,8 +147,8 @@ const Popup = () => {
     } else {
         return (
             <div style={{ minHeight: "200px" }}>
-                {showFetchStatus()}
                 <CardDisplayFrame cardNames={cardNames} resync={resyncCardNames}></CardDisplayFrame>
+                {showFetchStatus()}
             </div>
         )
     }
